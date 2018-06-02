b55453adf734637c3072d8eb9582829dbe1b20cf
Aidin Hassanzadeh <aidin.hassanzadeh@gmail.com>
2018-06-02 00:25:15 -0500

    Update notebooks/

--- hsipy/plot/__init__.py
+++ hsipy/plot/__init__.py
@@ -1,3 +1,4 @@
+
 import os
 import numpy as np
 import matplotlib.transforms as mtransforms
@@ -43,10 +44,141 @@ class StraightLine(lines.Line2D):
         self.text.draw(renderer)
 
 
-class PlotLoss(object):
-    """Documentation for PlotLoss
+class Scatter(object):
 
-    """
+    def __init__(self,
+                 marker: str = "o",
+                 markersize: int = 6,
+                 markeredgecolor: str = "black",
+                 markeredgewidth: float = 1,
+                 markerfacecolor: str = "black",
+                 fillstyle: str = "full",
+                 linestyle: str = "",
+                 alpha: float = .7,
+                 xlabel: str = "",
+                 ylabel: str = "",
+                 label: str = "",
+                 title: str = "",
+                 xlim: object = None,
+                 ylim: object = None,
+                 titlefontsize: int = 14,
+                 labelfontsize: int = 13,
+                 xtickfontsize: int = 12,
+                 ytickfontsize: int = 12,
+                 figsize: tuple = (7, 5),
+                 legend: bool = True,
+                 grid: bool = True,
+                 ax: object = None, ) -> object:
+
+        """
+
+        :param marker:
+        :param markersize:
+        :param markeredgecolor:
+        :param markeredgewidth:
+        :param markerfacecolor:
+        :param fillstyle:
+        :param linestyle:
+        :param alpha:
+        :param xlabel:
+        :param ylabel:
+        :param label:
+        :param title:
+        :param xlim:
+        :param ylim:
+        :param titlefontsize:
+        :param labelfontsize:
+        :param xtickfontsize:
+        :param ytickfontsize:
+        :param figsize:
+        :param legend:
+        :param grid:
+        :param ax:
+        """
+        super(Scatter, self).__init__()
+
+        self.marker = marker
+        self.markersize = markersize
+        self.markeredgecolor = markeredgecolor
+        self.markeredgewidth = markeredgewidth
+        self.markerfacecolor = markerfacecolor
+        self.fillstyle = fillstyle
+        self.linestyle = linestyle
+        self.alpha = alpha
+        self.xlabel = xlabel
+        self.ylabel = ylabel
+        self.label = label
+        self.title = title
+        self.xlim = xlim
+        self.ylim = ylim
+        self.titlefontsize = titlefontsize
+        self.labelfontsize = labelfontsize
+        self.xtickfontsize = xtickfontsize
+        self.ytickfontsize = ytickfontsize
+        self.figsize = figsize
+        self.legend = legend
+        self.grid = grid
+
+        # create figure/axis handler
+        if ax is None:
+            self.fig = plt.figure(figsize=self.figsize)
+            self.ax = self.fig.subplots(1, 1)
+
+    def __call__(self, x: object, y: object, *args, **kwargs):
+
+        # scatter plot
+        self.ax.plot(
+            x, y,
+            marker=self.marker,
+            markersize=self.markersize,
+            markeredgecolor=self.markeredgecolor,
+            markeredgewidth=self.markeredgewidth,
+            markerfacecolor=self.markerfacecolor,
+            fillstyle=self.fillstyle,
+            linestyle=self.linestyle,
+            alpha=self.alpha,
+            label=self.label,
+        )
+        # set title
+        _ = self.ax.set_title(
+            self.title,
+            fontsize=self.titlefontsize,
+            fontweight="bold"
+        )
+        # set axis labels
+        self.ax.set_xlabel(self.xlabel)
+        self.ax.set_ylabel(self.ylabel)
+        self.ax.xaxis.label.set_size(self.labelfontsize)
+        self.ax.yaxis.label.set_size(self.labelfontsize)
+
+        # set axis ticks
+        [tick.label.set_fontsize(self.xtickfontsize) for tick in self.ax.xaxis.get_major_ticks()]
+        [tick.label.set_fontsize(self.ytickfontsize) for tick in self.ax.yaxis.get_major_ticks()]
+
+        # set axis range limits
+        if self.xlim is not None:
+            self.ax.set_xlim(self.xlim)
+        if self.ylim is not None:
+            self.ax.set_ylim(self.ylim)
+
+        # set legend
+        if self.legend and (self.label is not ""):
+            self.ax.legend()
+
+        # set grid
+        if self.grid:
+            self.ax.grid(
+                True,
+                color="grey",
+                linestyle=":",
+                linewidth=1.5,
+                alpha=0.5)
+
+        # set tight layout
+        plt.tight_layout()
+
+
+class PlotLoss(object):
 
     def __init__(self,
                  xscale=None,
@@ -75,6 +207,9 @@ class PlotLoss(object):
 
     def __call__(self, loss, label=None, color=None):
         """
+        :param loss:
+        :param label:
+        :param color:
         """
         n_records = len(loss)
         if not hasattr(self, 'fig'):
